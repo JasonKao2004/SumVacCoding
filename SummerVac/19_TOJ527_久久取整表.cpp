9df25a2ec4536291f2d843a57583cfdd891c1eee
Jason Kao <jasonkao.2004@gmail.com>
2023-07-10 22:54:40 +0800

    upload files quest 19

---
+++ SummerVac/19_TOJ527_久久取整表.cpp
@@ -0,0 +1,9 @@
+#include <iostream>             /* 87 -> 90 ; 90 -> 99  取9的倍數 */
+using namespace std;
+
+int main (){
+int n, i;
+cin >> n ;
+    for ( i = 0 ; i - 9 <= n ; i = i + 9 ){  }
+cout << i - 9 << endl ;
+}
